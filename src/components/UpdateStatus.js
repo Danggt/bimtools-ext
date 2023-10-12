d695aeb11ac7563a5ac3643ea87f4e59cd9e92c5
Danggt <dgt.icebsolutions@gmail.com>
2023-10-12 20:41:55 +0700

    update fabrication status

--- src/components/UpdateStatus.js
+++ src/components/UpdateStatus.js
@@ -233,6 +233,8 @@ const UpdateStatus = () => {
       });
   };
   const representingStatus = (status) => {
+    message.info("Representing in progress..",2.5)
+    //console.log(data.data[0].objectRuntimeIds)
     //Get current project
     api.then((tcapi) => {
       tcapi.project.getProject().then((result) => {
@@ -241,131 +243,205 @@ const UpdateStatus = () => {
     });
 
     api.then(async (tcapi) => {
+      console.log(tcapi)
       const objects = await tcapi.viewer.getObjects();
-      const modelId = objects[0].modelId;
-      const objects_id = objects[0].objects.map((x) => x.id);
+      console.log(objects)
+      objects.forEach(async model => {
+        const modelId = model.modelId;
+        const objects_id = model.objects.map((x) => x.id);
+        console.log(objects_id)
 
-      const external_ids = await tcapi.viewer.convertToObjectIds(
-        modelId,
-        objects_id
-      );
-      const original_ids = external_ids.map(x=>x.replace('$',''))
-      const properties = await tcapi.viewer.getObjectProperties(
-        modelId,
-        objects_id
-      );
+        // tcapi.viewer.setObjectState({
+        //   modelObjectIds: [
+        //     {
+        //       modelId: modelId,
+        //       objectRuntimeIds: data.data[0].objectRuntimeIds,
+        //     },
+        //   ],
+        // }, {
+        //   color: "#d1cdc7",
+        //   visible: true,
+        // })
 
-      const url = `https://europe.tcstatus.tekla.com/statusapi/1.0`;
-      const res_status_token = await axios.post(
-        `${url}/auth/token`,
-        {},
-        {
-          headers: {
-            Authorization: `Bearer ${accesstoken}`,
-          },
-        }
-      );
-      const status_token = res_status_token.data;
-      //Get action status
-      const res_statuses = await axios.get(
-        `${url}/projects/${projectId}/statusactions`,
-        {
-          headers: {
-            Authorization: `Bearer ${status_token}`,
-          },
-        }
-      );
+        const external_ids = await tcapi.viewer.convertToObjectIds(
+          modelId,
+          objects_id
+        );
+        console.log(external_ids)
 
-      const statuses = res_statuses.data.map((x) => {
-        if (x.name === "1) Planning") {
-          return {
-            id: x.id,
-            color: "#F1C40F",
-            name: x.name,
-          };
-        } else if (x.name === "2) In Fabrication") {
-          return {
-            id: x.id,
-            color: "#1ABC9C",
-            name: x.name,
-          };
-        } else if (x.name === "3) In Treatment") {
-          return {
-            id: x.id,
-            color: "#2980B9",
-            name: x.name,
-          };
-        } else if (x.name === "3.1) OSWI Completed") {
-          return {
-            id: x.id,
-            color: "#9B59B6",
-            name: x.name,
-          };
-        } else if (x.name === "4) At Dispatch") {
-          return {
-            id: x.id,
-            color: "#E74C3C",
-            name: x.name,
-          };
-        } else if (x.name === "5) Shipped") {
-          return {
-            id: x.id,
-            color: "#B9770E",
-            name: x.name,
-          };
-        } else if (x.name === "Status1") {
-          return {
-            id: x.id,
-            color: "#25cf0e",
-            name: x.name,
-          };
-        } else if (x.name === "Status2") {
-          return {
-            id: x.id,
-            color: "#cf0e18",
-            name: x.name,
-          };
-        }
-      });
-      const current_status = statuses.filter(
-        (x) => typeof x !== "undefined" && x.name === status
-      )[0];
-      //Get element statuses
-      const res_status = await axios.get(
-        `${url}/projects/${projectId}/status?statusActionId=${current_status.id}`,
-        {
-          headers: {
-            Authorization: `Bearer ${status_token}`,
-          },
-        }
-      );
-      console.log(res_status.data);
-      const ids = res_status.data.map((x) => x.objectId);
-      console.log(ids);
-      let model_obj_ids = [];
-      ids.forEach((id) => {
-        const index = original_ids.indexOf(id);
-        if (index < 0) return;
-        model_obj_ids.push(index);
-      });
-      console.log(model_obj_ids)
+        const original_ids = external_ids.map(x => x.replace('$', ''))
+        // const properties = await tcapi.viewer.getObjectProperties(
+        //   modelId,
+        //   data.data[0].objectRuntimeIds
+        // );
+        // console.log(properties)
 
-      console.log(current_status);
-      console.log(model_obj_ids)
-      tcapi.viewer.setObjectState(
-        {
+        const url = `https://europe.tcstatus.tekla.com/statusapi/1.0`;
+        const res_status_token = await axios.post(
+          `${url}/auth/token`,
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${accesstoken}`,
+            },
+          }
+        );
+        const status_token = res_status_token.data;
+        //Get action status
+        const res_statuses = await axios.get(
+          `${url}/projects/${projectId}/statusactions`,
+          {
+            headers: {
+              Authorization: `Bearer ${status_token}`,
+            },
+          }
+        );
+        const statuses = res_statuses.data.map((x) => {
+          if (x.name === "1) Planning") {
+            return {
+              id: x.id,
+              color: "#F1C40F",
+              name: x.name,
+            };
+          } else if (x.name === "2) In Fabrication") {
+            return {
+              id: x.id,
+              color: "#1ABC9C",
+              name: x.name,
+            };
+          } else if (x.name === "3) In Treatment") {
+            return {
+              id: x.id,
+              color: "#2980B9",
+              name: x.name,
+            };
+          } else if (x.name === "3.1) OSWI Completed") {
+            return {
+              id: x.id,
+              color: "#9B59B6",
+              name: x.name,
+            };
+          } else if (x.name === "4) At Dispatch") {
+            return {
+              id: x.id,
+              color: "#E74C3C",
+              name: x.name,
+            };
+          } else if (x.name === "5) Shipped") {
+            return {
+              id: x.id,
+              color: "#B9770E",
+              name: x.name,
+            };
+          } else if (x.name === "Status1") {
+            return {
+              id: x.id,
+              color: "#25cf0e",
+              name: x.name,
+            };
+          } else if (x.name === "Status2") {
+            return {
+              id: x.id,
+              color: "#cf0e18",
+              name: x.name,
+            };
+          }
+        });
+
+        const current_status = statuses.filter(
+          (x) => typeof x !== "undefined" && x.name === status
+        )[0];
+        //Get element statuses
+        const res_status = await axios.get(
+          `${url}/projects/${projectId}/status?statusActionId=${current_status.id}`,
+          {
+            headers: {
+              Authorization: `Bearer ${status_token}`,
+            },
+          }
+        );
+        console.log(res_status.data);
+        const ids = res_status.data.map((x) => x.objectId);
+        console.log(ids);
+        let model_obj_ids = [];
+        ids.forEach((id) => {
+          const index = original_ids.indexOf(id);
+          if (index < 0) return;
+          model_obj_ids.push(index);
+        });
+        console.log(model_obj_ids)
+
+        console.log(current_status);
+        // tcapi.viewer.setSelection({
+        //   modelObjectIds: [
+        //     {
+        //       modelId: modelId,
+        //       objectRuntimeIds: [2001],
+        //     },
+        //   ],
+        // }, "add")
+        tcapi.viewer.setObjectState({
           modelObjectIds: [
             {
               modelId: modelId,
               objectRuntimeIds: model_obj_ids,
             },
           ],
-        },
-        {
+        }, {
           color: current_status.color,
           visible: true,
-        }
-      );
+        })
+
+        // tcapi.viewer.setObjectState(
+        //   {
+        //     modelObjectIds: [
+        //       {
+        //         modelId: modelId,
+        //         objectRuntimeIds: [2001],
+        //         parameter:{
+        //           class:'IFCELEMENTASSEMBLY'
+        //         }
+        //       },
+        //     ],
+        //   },
+        //   {
+        //     color: current_status.color,
+        //     visible: true,
+        //   }
+        // );
+      })
+    });
+    message.success("Action has been done",5)
+  };
+
+  const resetColor = () => {
+    api.then((tcapi) => {
+      tcapi.project.getProject().then((result) => {
+        setProjectId(result.id);
+      });
+    });
+
+    api.then(async (tcapi) => {
+      console.log(tcapi)
+      const objects = await tcapi.viewer.getObjects();
+      console.log(objects)
+      objects.forEach(async model => {
+        const modelId = model.modelId;
+        const objects_id = model.objects.map((x) => x.id);
+        console.log(objects_id)
+
+        tcapi.viewer.setObjectState({
+          modelObjectIds: [
+            {
+              modelId: modelId,
+              objectRuntimeIds: objects_id,
+            },
+          ],
+        }, {
+          color: "reset",
+        })
+
+      })
     });
   };
   return (
@@ -458,11 +534,9 @@ const UpdateStatus = () => {
             style={{ background: "#B9770E" }}
             onClick={() => representingStatus(`5) Shipped`)}
           >{`5) Shipped`}</Button>
-          {/* <Button type="primary" onClick={()=>{
-
-          }}>
+          <Button type="primary" onClick={resetColor}>
             Reset Color
-          </Button> */}
+          </Button>
           {/* <Button
             type="primary"
             onClick={() => {
